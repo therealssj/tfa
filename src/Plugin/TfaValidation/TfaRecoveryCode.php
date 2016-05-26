1c0f0f08f01eef12f6c356a5993369be6027734a
therealssj <mehul.guptagm@gmail.com>
2016-05-27 00:12:36 +0530

    TFA Validation Shift 1

---
+++ src/Plugin/TfaValidation/TfaRecoveryCode.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * @file TfaRecoveryCode class
+ */
+
+namespace Drupal\tfa\Plugin\TfaValidation;
+
+use Drupal\tfa\Plugin\TfaBasePlugin;
+use Drupal\tfa\Plugin\TfaValidationInterface;
+use Drupal\tfa\Plugin\TfaSetupInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * @TfaValidation(
+ *   id = "tfa_recovery_code",
+ *   label = @Translation("TFA Recovery Code Validation"),
+ *   description = @Translation("TFA Recovery Code Validation Plugin")
+ * )
+ */
+class TfaRecoveryCode extends TfaBasePlugin implements TfaValidationInterface {
+
+  /**
+   * @var string
+   */
+  protected $usedCode;
+
+  public function __construct(array $context) {
+    parent::__construct($context);
+    // Set in settings.php.
+    $this->encryptionKey = \Drupal::config('tfa_basic.settings')->get('secret_key');
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::ready()
+   */
+  public function ready() {
+    $codes = $this->getCodes();
+    return !empty($codes);
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::getForm()
+   */
+  public function getForm(array $form, FormStateInterface $form_state) {
+    $form['recover'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Enter one of your recovery codes'),
+      '#required' => TRUE,
+      '#description' => t('Recovery codes were generated when you first set up TFA. Format: XXX XX XXX'),
+      '#attributes' => array('autocomplete' => 'off'),
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['login'] = array(
+      '#type' => 'submit',
+      '#value' => t('Verify'),
+    );
+    return $form;
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::validateForm()
+   */
+  public function validateForm(array $form, FormStateInterface $form_state) {
+    return $this->validate($form_state['values']['recover']);
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::finalize()
+   */
+  public function finalize() {
+    // Mark code as used.
+    if ($this->usedCode) {
+      $num = db_update('tfa_recovery_code')
+        ->fields(array('used' => REQUEST_TIME))
+        ->condition('id', $this->usedCode)
+        ->condition('uid', $this->context['uid'])
+        ->execute();
+      if ($num) {
+        watchdog('tfa_basic', 'Used TFA recovery code !id by user !uid', array('!id' => $this->usedCode, '!uid' => $this->context['uid']), WATCHDOG_NOTICE);
+      }
+    }
+  }
+
+  /**
+   * Get unused recovery codes.
+   *
+   * @todo consider returning used codes so validate() can error with
+   * appropriate message
+   *
+   * @return array
+   *   Array of codes indexed by ID.
+   */
+  public function getCodes() {
+    // Lookup codes for account and decrypt.
+    $codes = array();
+    $result = db_query("SELECT id, code FROM {tfa_recovery_code} WHERE uid = :uid AND used = 0", array(':uid' => $this->context['uid']));
+    if (!empty($result)) {
+      foreach ($result as $data) {
+        $encrypted = base64_decode($data->code);
+        // trim() prevents extraneous escape characters.
+        $code = trim($this->decrypt($encrypted));
+        if (!empty($code)) {
+          $codes[$data->id] = $code;
+        }
+      }
+    }
+    return $codes;
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::validate()
+   */
+  protected function validate($code) {
+    $this->isValid = FALSE;
+    // Get codes and compare.
+    $codes = $this->getCodes();
+    if (empty($codes)) {
+      $this->errorMessages['code'] = t('You have no unused codes available.');
+      return FALSE;
+    }
+    // Remove empty spaces.
+    $code = str_replace(' ', '', $code);
+    foreach ($codes as $id => $stored) {
+      // Remove spaces from stored code.
+      if (str_replace(' ', '', $stored) === $code) {
+        $this->isValid = TRUE;
+        $this->usedCode = $id;
+        return $this->isValid;
+      }
+    }
+    $this->errorMessages['code'] = t('Invalid recovery code.');
+    return $this->isValid;
+  }
+
+}
---
+++ src/Plugin/TfaValidation/TfaTotp.php
@@ -0,0 +1,182 @@
+<?php
+
+/**
+ * @file TfaTOTP class
+ */
+
+namespace Drupal\tfa\Plugin\TfaValidation;
+
+use Drupal\tfa\Plugin\TfaBasePlugin;
+use Drupal\tfa\Plugin\TfaValidationInterface;
+use Drupal\tfa\Plugin\TfaSetupInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\Entity\User;
+use Drupal\Component\Utility\Crypt;
+use Drupal\Core\Site\Settings;
+use PHPGangsta_GoogleAuthenticator as GoogleAuthenticator;
+
+/**
+ * @TfaValidation(
+ *   id = "tfa_totp",
+ *   label = @Translation("TFA Toptp Validation"),
+ *   description = @Translation("TFA Toptp Validation Plugin")
+ * )
+ */
+class TfaTotp extends TfaBasePlugin implements TfaValidationInterface {
+
+  /**
+   * @var PHPGangsta_GoogleAuthenticator
+   */
+  protected $ga;
+
+  /**
+   * @var int
+   */
+  protected $timeSkew;
+
+  /**
+   * @var bool
+   */
+  protected $alreadyAccepted;
+
+
+  /**
+   * @copydoc TfaBasePlugin::__construct()
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->ga = new GoogleAuthenticator();
+    // Allow codes within tolerance range of 3 * 30 second units.
+    $this->timeSkew = \Drupal::config('tfa_basic.settings')->get('time_skew');
+    // Recommended: set variable tfa_totp_secret_key in settings.php.
+    $this->encryptionKey = \Drupal::config('tfa_basic.settings')->get('secret_key');
+    $this->alreadyAccepted = FALSE;
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::ready()
+   */
+  public function ready() {
+    return ($this->getSeed() !== FALSE);
+  }
+
+  /**
+   * @copydoc TfaValidationPluginInterface::getForm()
+   */
+  public function getForm(array $form, FormStateInterface $form_state) {
+    $form['code'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Application verification code'),
+      '#description' => t('Verification code is application generated and @length digits long.', array('@length' => $this->codeLength)),
+      '#required' => TRUE,
+      '#attributes' => array('autocomplete' => 'off'),
+    );
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['login'] = array(
+      '#type' => 'submit',
+      '#value' => t('Verify'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * @copydoc TfaValidationPluginInterface::validateForm()
+   */
+  public function validateForm(array $form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    //dpm($values);
+    if (!$this->validate($values['code'])) {
+      $form_state->setErrorByName('code', t('Invalid application code. Please try again.'));
+      if ($this->alreadyAccepted) {
+        $form_state->setErrorByName('code', t('Invalid code, it was recently used for a login. Please wait for the application to generate a new code.'));
+      }
+    }
+    else {
+      // Store accepted code to prevent replay attacks.
+      $this->storeAcceptedCode($values['code']);
+    }
+  }
+
+  /**
+   * @copydoc TfaBasePlugin::validate()
+   */
+  protected function validate($code) {
+    // Strip whitespace.
+    $code = preg_replace('/\s+/', '', $code);
+    if ($this->alreadyAcceptedCode($code)) {
+      $this->isValid = FALSE;
+    }
+    else {
+      // Get OTP seed.
+      $seed = $this->getSeed();
+      $this->isValid = ($seed && $this->ga->verifyCode($seed, $code, $this->timeSkew));
+    }
+    return $this->isValid;
+  }
+
+  /**
+   * @param string $code
+   */
+  protected function storeAcceptedCode($code) {
+    $code = preg_replace('/\s+/', '', $code);
+    $hash = hash('sha1', Settings::getHashSalt() . $code);
+    db_insert('tfa_accepted_code')
+      ->fields(array(
+        'uid' => $this->context['uid'],
+        'code_hash' => $hash,
+        'time_accepted' => REQUEST_TIME,
+      ))
+      ->execute();
+  }
+
+  /**
+   * Whether code has recently been accepted.
+   *
+   * @param string $code
+   * @return bool
+   */
+  protected function alreadyAcceptedCode($code) {
+    $hash = hash('sha1', Settings::getHashSalt() . $code);
+    $result = db_query(
+      "SELECT code_hash FROM {tfa_accepted_code} WHERE uid = :uid AND code_hash = :code",
+      array(':uid' => $this->context['uid'], ':code' => $hash)
+    )->fetchAssoc();
+    if (!empty($result)) {
+      $this->alreadyAccepted = TRUE;
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * Get seed for this account.
+   *
+   * @return string Decrypted account OTP seed or FALSE if none exists.
+   */
+  protected function getSeed() {
+    // Lookup seed for account and decrypt.
+    $result = db_query("SELECT seed FROM {tfa_totp_seed} WHERE uid = :uid", array(':uid' => $this->context['uid']))->fetchAssoc();
+    if (!empty($result)) {
+      $encrypted = base64_decode($result['seed']);
+      $seed = $this->decrypt($encrypted);
+      if (!empty($seed)) {
+        return $seed;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Delete users seeds.
+   *
+   * @return int
+   */
+  public function deleteSeed() {
+    $query = db_delete('tfa_totp_seed')
+      ->condition('uid', \Drupal::currentUser()->id());
+
+    return $query->execute();
+  }
+}
